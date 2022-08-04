efae7a083f63b4ac7f15736bc30cda6371dd6680
Boyan Deshev <grievous@abv.bg>
2022-08-04 14:14:46 +0300

    added a printGameState function

--- main.go
+++ main.go
@@ -18,6 +18,29 @@ var dictionary = []string{
 
 func main() {
 	rand.Seed(time.Now().UnixNano())
-	targetWord := dictionary[rand.Intn(len(dictionary))]
+	targetWord := getRandomWord()
+	guessedLetters := map[rune]bool{}
+	guessedLetters[rune(targetWord[0])] = true
+	guessedLetters[rune(targetWord[len(targetWord)-1])] = true
 	fmt.Println(targetWord)
+
+	printGameState(targetWord, guessedLetters)
+}
+
+func getRandomWord() string {
+	targetWord := dictionary[rand.Intn(len(dictionary))]
+	return targetWord
+}
+func printGameState(targetWord string, guessedLetters map[rune]bool) {
+	for _, ch := range targetWord {
+		if ch == ' ' {
+			fmt.Print(" ")
+		} else if guessedLetters[ch] {
+			fmt.Printf("%c", ch)
+		} else {
+			fmt.Printf("_")
+		}
+		fmt.Print(" ")
+	}
+	fmt.Println()
 }
