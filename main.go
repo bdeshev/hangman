136ba8312791f719f94c66251d3756ba81260849
Boyan Deshev <grievous@abv.bg>
2022-08-08 16:30:29 +0300

    code with error

--- main.go
+++ main.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"math/rand"
 	"time"
+	"unicode"
 )
 
 var dictionary = []string{
@@ -19,12 +21,17 @@ var dictionary = []string{
 func main() {
 	rand.Seed(time.Now().UnixNano())
 	targetWord := getRandomWord()
-	guessedLetters := map[rune]bool{}
-	guessedLetters[rune(targetWord[0])] = true
-	guessedLetters[rune(targetWord[len(targetWord)-1])] = true
-	fmt.Println(targetWord)
+	guessedLetters := initializeGuessedWord(targetWord)
 
 	printGameState(targetWord, guessedLetters)
+}
+
+func initializeGuessedWord(targetWord string) map[rune]bool {
+	guessedLetters := map[rune]bool{}
+	guessedLetters[unicode.ToLower(rune(targetWord[0]))] = true
+	guessedLetters[unicode.ToLower(rune(targetWord[len(targetWord)-1]))] = true
+
+	return guessedLetters
 }
 
 func getRandomWord() string {
@@ -35,7 +42,7 @@ func printGameState(targetWord string, guessedLetters map[rune]bool) {
 	for _, ch := range targetWord {
 		if ch == ' ' {
 			fmt.Print(" ")
-		} else if guessedLetters[ch] {
+		} else if guessedLetters[unicode.ToLower(ch)] {
 			fmt.Printf("%c", ch)
 		} else {
 			fmt.Printf("_")
@@ -43,4 +50,15 @@ func printGameState(targetWord string, guessedLetters map[rune]bool) {
 		fmt.Print(" ")
 	}
 	fmt.Println()
+	fmt.Println()
+	fmt.Println(getHangmanDrawing(1))
+}
+
+func getHangmanDrawing(hangmanState int) string {
+	data, err := ioutil.ReadFile("states/hangman1")
+	if err != nil {
+		panic(err)
+	}
+
+	return string(data)
 }
